181b13c5eb48c189ea5e572f38d77d212db4004c
Vansh Kashyap <145321719+Vansh-kash2023@users.noreply.github.com>
2024-10-02 12:16:31 +0530

    kartik utsav card added

--- src/views/Events.jsx
+++ src/views/Events.jsx
@@ -31,97 +31,97 @@ import onstage from '../components/Events/eventimgs/onstage.jpg'
 const Events = () => {
   const events = [
     {
-      title: "Dance",
-      category: "dance",
+      title: "Kartik Utsav",
+      category: "Dance",
       imgUrl:dance,
       category_id: 7
     },
-    {
-      title: "Music",
-      category: "sing",
-      imgUrl:music,
-      category_id: 3
+    // {
+    //   title: "Music",
+    //   category: "sing",
+    //   imgUrl:music,
+    //   category_id: 3
 
-    },
-    {
-      title: "Online Games",
-      category: "game",
-      imgUrl:
-        "https://cdn.dribbble.com/userupload/3348061/file/original-5fd000ed98af552463352a4e82773576.png?compress=1&resize=752x",
-        category_id: 1
-    },
+    // },
+    // {
+    //   title: "Online Games",
+    //   category: "game",
+    //   imgUrl:
+    //     "https://cdn.dribbble.com/userupload/3348061/file/original-5fd000ed98af552463352a4e82773576.png?compress=1&resize=752x",
+    //     category_id: 1
+    // },
 
-    {
-      title: "Drama",
-      category: "drama",
-      imgUrl:drama,
-      category_id: 2
-    },
-    {
-      title: "Fashion",
-      category: "Fashion",
-      imgUrl:fashion,
-      category_id: 4
-    },
-    {
-      title: "Entertainment",
-      category: "game",
-      imgUrl:entertainment,
-      category_id: 5
+    // {
+    //   title: "Drama",
+    //   category: "drama",
+    //   imgUrl:drama,
+    //   category_id: 2
+    // },
+    // {
+    //   title: "Fashion",
+    //   category: "Fashion",
+    //   imgUrl:fashion,
+    //   category_id: 4
+    // },
+    // {
+    //   title: "Entertainment",
+    //   category: "game",
+    //   imgUrl:entertainment,
+    //   category_id: 5
 
-    },
+    // },
 
-    {
-      title: "Photography",
-      category: "dance",
-      imgUrl:photo,
-      category_id: 6
-    },
-    {
-      title: "Film",
-      category: "sing",
-      imgUrl:film,
-      category_id: 8
-    },
-    {
-      title: "Arts",
-      category: "game",
-      imgUrl:art,
-      category_id: 9
-    },
+    // {
+    //   title: "Photography",
+    //   category: "dance",
+    //   imgUrl:photo,
+    //   category_id: 6
+    // },
+    // {
+    //   title: "Film",
+    //   category: "sing",
+    //   imgUrl:film,
+    //   category_id: 8
+    // },
+    // {
+    //   title: "Arts",
+    //   category: "game",
+    //   imgUrl:art,
+    //   category_id: 9
+    // },
 
-    {
-      title: "Sports",
-      category: "sports",
-      imgUrl:sports,
-      category_id: 10
-    },
-    {
-      title: "Literature",
-      category: "literature",
-      imgUrl:literature,
-      category_id: 11
-    },
-    {
-      title: "Fun Street",
-      category: "fun",
-      imgUrl:quiz,
-      category_id: 12
-    },
+    // {
+    //   title: "Sports",
+    //   category: "sports",
+    //   imgUrl:sports,
+    //   category_id: 10
+    // },
+    // {
+    //   title: "Literature",
+    //   category: "literature",
+    //   imgUrl:literature,
+    //   category_id: 11
+    // },
+    // {
+    //   title: "Fun Street",
+    //   category: "fun",
+    //   imgUrl:quiz,
+    //   category_id: 12
+    // },
 
-    {
-      title: "On Stage",
-      category: "dance",
-      imgUrl:onstage,
-      category_id: 13
-    },
+    // {
+    //   title: "On Stage",
+    //   category: "dance",
+    //   imgUrl:onstage,
+    //   category_id: 13
+    // },
   ];
 
   return (
     <div id="events">
       <div id="content" className="flex flex-col items-center py-10 gap-y-4">
         <div class="main text-6xl">
-          <span class="socod">EVENTS</span>
+          <span class="text-white">UPCOMING EVENTS</span>
         </div>
 
         <motion.div
